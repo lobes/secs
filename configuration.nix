68d74484faacdfb7b0ab528c8ada35394e38f111
lobes <samuel.t.giles@gmail.com>
2023-12-25 17:24:10 +1100

    nfi what I'm doing

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,18 @@
       system = "aarch64-linux";
       modules = [
         "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        { config.system.stateVersion = "22.05"; }
+        {
+          config = {
+            system = {
+              # Disable zstd compression
+              build.sdImage.compressImage = false;
+
+              # ... other configs
+
+              stateVersion = "23.10";
+            };
+          };
+        }
       ];
     };
   };
